1081997efb5ba0ea09632f40a7188f7cee9843cf
maicon_dionatans <maicon_dionatans@SNRSL1ELBIET031>
2016-11-16 09:42:12 -0200

    Parte home quase pronta

--- index.php
+++ index.php
@@ -26,18 +26,30 @@ and open the template in the editor.
             </div>
             <div id="content">
                 <div id="content1">
-                    <h1>
+                    <h1 align="center">
+                        <big>
                         Bem vindo ao Site!
+                        </big>
                     </h1>
-                    <p>
+                    <br>
+                    <p class="parag">
                         Este site foi criado por um certo grupo de estudantes do curso de Aprendizagem
                         Industrial em Informática, que tinham como intúito criar uma empresa especializada em produtos e assistência
                         relacionados a área de informática para poder colocar em prática os conhecimentos obtidos ao longo do curso.
                     </p>
                     <br>
+                    <p class="parag">
+                        Neste site poderás encontrar os mais variados tipos de produtos para seu computador, que vão desde peças  
+                        para "tunar" seu hardware caso você seja um explendido jogador, itens para apresentações para sua escola,
+                        serviços de recuperação de dados e criação de softwares melhorar o desempenho de sua empresa.
+                    </p>
+                    <br>
+                    <p class="parag">
+                        O site é fácil de navegar, então fique a vontade para procurar o serviço que precisas.
+                    </p>
                 </div>
                 <div id="content2">
-
+                    <img src="img/home.jpg"/>
                 </div>
             </div>
             <div id="footer"></div>

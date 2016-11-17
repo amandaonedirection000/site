1caf014f410286f179775650a78aeda513368e65
william_branco <william_branco@SNRSL1ELBIET035>
2016-11-17 10:27:48 -0200

    finalização da aba produtos

--- produtos.php
+++ produtos.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -26,19 +20,127 @@ and open the template in the editor.
             </div>
             <div id="content">
                 <div id="content1">
-
+                    <div class=""titulo>
+                        <h2> Conheça Nossos Produtos</h2>
+                    </div>
+                    <hr> 
+                    <div class="promocao">
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/iphone 5s.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                Iphone 5s Apple <br>
+                                R$ 1.999,00
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/sansung.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                Tablet Samsung Galaxy Tab A <br>
+                                R$ 1.599,00
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/not apple.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                Macbook Air MJVG2BZ/A <br>
+                                R$ 4.966,15
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/IMAC.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                IMac Apple MF886BZ/A <br>
+                                R$ 15.816,80
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/55.png" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                Macbook Pro MJLT2BZ/A <br>
+                                R$ 5.399,15
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/ipad.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                 Ipad mini Me280br/A<br>
+                                R$ 2.184,90
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/download.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                IPad Pro Apple <br>
+                                R$ 7.719,90
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/pen-drive.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                USB 2.0 black <br>
+                                R$16,63
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/impressora.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                               Impressora 7110 WIFI/ A3 BIVOLT <br>
+                                R$511,48
+                            </div> 
+                        </div> 
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/mouse.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                               Mouse KMO-375BK  <br>
+                                R$40,47
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/6s.jpeg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                                 iPhone 6s Apple<br>
+                                R$ 4.299.00
+                            </div> 
+                        </div>
+                        <div class="img">
+                            <a href="#">
+                                <img src="img/controle.jpg" width="300" height="200"/>
+                            </a>
+                            <div class="desc">
+                               Controle sem fio Xbox 360 <br>
+                                R$ 147,90
+                            </div> 
+                        </div>
+                    </div>
                 </div>
-                <div id="content2">
-
+                <div id="footer">
+                    <p>2016 &copy; - Todos os direitos reservados.</p>
                 </div>
             </div>
-            <div id="footer">
-                <p>2016 &copy; - Todos os direitos reservados.</p>
-            </div>
-        </div>
-
-        <?php
-        // put your code here
-        ?>
+            <?php
+            // put your code here
+            ?>
     </body>
 </html>

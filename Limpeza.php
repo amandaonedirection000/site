a339a961451484923223a7c1b346d7ad16fc3412
william_branco <william_branco@SNRSL1ELBIET035>
2016-11-23 11:22:50 -0200

    Criado2

---
+++ Limpeza.php
@@ -0,0 +1,118 @@
+
+<div id="content5">
+    <div class="titulo">
+        <h1> A seguir mostraremos a vocês como fazer a limpeza de um computador corretamente.</h1>
+
+    </div>
+    <hr> 
+    <div class="promocao">
+        <div class="img">
+            <a href="#">
+                <img src="img/1.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Iphone 5s Apple <br>
+                R$ 1.999,00
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/2.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Tablet Samsung Galaxy Tab A <br>
+                R$ 1.599,00
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/3.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Macbook Air MJVG2BZ/A <br>
+                R$ 4.966,15
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/4.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                IMac Apple MF886BZ/A <br>
+                R$ 15.816,80
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/55.png" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Macbook Pro MJLT2BZ/A <br>
+                R$ 5.399,15
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/ipad.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Ipad mini Me280br/A<br>
+                R$ 2.184,90
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/download.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                IPad Pro Apple <br>
+                R$ 7.719,90
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/pen-drive.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                USB 2.0 black <br>
+                R$16,63
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/impressora.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Impressora 7110 WIFI/ A3 BIVOLT <br>
+                R$511,48
+            </div> 
+        </div> 
+        <div class="img">
+            <a href="#">
+                <img src="img/mouse.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Mouse KMO-375BK  <br>
+                R$40,47
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/6s.jpeg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                iPhone 6s Apple<br>
+                R$ 4.299.00
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/controle.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Controle sem fio Xbox 360 <br>
+                R$ 147,90
+            </div> 
+        </div>
+    </div>
+</div>

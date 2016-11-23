f6ce88a6785dd6b6f9223f11ef77ddd364bcbba1
amanda_hilda <amanda_hilda@SNRSL1ELBIET034>
2016-11-23 10:33:50 -0200

    links

--- faq.php
+++ faq.php
@@ -6,46 +6,49 @@
     </h1>
     <br>
     <p class="parag">
-    <p>   1- Quais os critérios mais influentes na hora de comprar um smartphone? <br>
+    <p> <b>  1- Quais os critérios mais influentes na hora de comprar um smartphone? </b> <br>
         Verificar a memória ram, se ela supre as necessidades do usuário.</p>
 
 <br>
-<p> 2- Qual o melhor antivírus para meu micro? <br>
-    Antivírus pago: Kaspersky e F-Secure <br>
-Antivírus gratuito: 360 Total Security e Avira Free Antivirus</p>
+<p> <b> 2- Qual o melhor antivírus para meu micro? </b><br>
+    Antivírus pago: <a href="http://store.kaspersky.com/store/kasperbr/pt_BR/custom/pbPage.antivirus?affiliate=drppc_gbs_DR_LP&ksid=kw81974&ksprof_id=64&ksaffcode=81974&ksdevice=c&gclid=CPn69ZruvtACFUOAkQodDzgHvQ" target="_blank"> Kaspersky </a> e <a href="https://www.f-secure.com/pt_BR/web/business_br/protection-service-for-business?chl=sea&src=gsrc&itm=brand&ecid=6634&pcid=6634&gclid=CJe5yfruvtACFRcGkQodwy0IUg"  target="_blank"> F-Secure </a> <br>
+    Antivírus gratuito: <a href="http://www.360totalsecurity.com/pt/" target="_blank" > 360 Total Security </a> e <a href="https://www.avira.com/pt-br/download/product/avira-free-antivirus" target="_blank" > Avira Free Antivirus </a></p>
 
 <br>
-<p>3-  Como criar uma página de internet? <br>
-    É preciso ter conhecimentos básicos de programação e raciocínio lógico. </p>
+<p> <b> 3-  Como criar uma página de internet? </b> <br>
+    É preciso ter conhecimentos básicos de programação e raciocínio lógico.<br>
+    Consulte o site para maiores explicações:
+    <a href="http://www.w3schools.com/" target="_blank" >W3schools</a>
+</p>
 
 <br>
-<p> 4- Por que há computadores com os mesmo componentes e com fontes diferentes? <br>
+<p> <b> 4- Por que há computadores com os mesmo componentes e com fontes diferentes? </b> <br>
     Devido as regiões e a distribuição de energia. E vai do fabricante escolher uma fonte chaveada ou bivolt. </p>
 <br>
 
-<p> 5- O que é bug? <br>
+<p> <b> 5- O que é bug? </b> <br>
     Falha ou erro no código de um programa que provoca seu mau funcionamento. </p>
 
 <br>
-<p> 6- Por que fazer backup? <br>
+<p> <b> 6- Por que fazer backup? </b> <br>
     O backup existe para auxiliar na prevenção de perda de dados, como arquivos apagados acidentalmente por falha física ou humana. Caso o sistema seja pequeno, pode-se considerar um método pequeno, de backups 'full', ou seja, que vários dados sejam copiados todos os dias. O inconveniente é a demora desse tipo de backup.</p>
 
 <br>
-<p>7- O que é um upgrade? <br>
+<p> <b> 7- O que é um upgrade? </b> <br>
     Atualização dos componentes do hardware ou do software, de um computador.</p>
 
 <br>
-<p> 8- O que é Correio Eletrônico? <br>
+<p> <b> 8- O que é Correio Eletrônico?</b> <br>
 O correio eletrônico ou e-mail é uma ferramenta usada para enviar e receber mensagens de maneira instantânea através da Internet.
 </p>
 <br>
 
-<p> 9- O que é hardware? <br>
+<p> <b> 9- O que é hardware? </b> <br>
 O hardware é a parte que você pode ver do computador, ou seja, todos os componentes da sua estrutura física. A tela, o teclado, o gabinete e o mouse fazem parte do hardware do seu computador.
 </p>
 <br>
 
-<p>10- O que é software? <br>
+<p> <b> 10- O que é software? </b> <br>
     São os programas que permitem realizar atividades específicas em um computador. Por exemplo, os programas como Word, Excel, Power Point, os navegadores, os jogos, os sistemas operacionais, entre outros. </p>
 
 

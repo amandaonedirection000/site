effe794b6c957ac11796a9ae48a94b2563e7fc66
amanda_hilda <amanda_hilda@SNRSL1ELBIET034>
2016-11-24 10:47:39 -0200

    limpeza pronta

--- Limpeza.php
+++ Limpeza.php
@@ -1,325 +1,166 @@
  
-<div id="content5">
-    <div class="titulo">
-        <h1> A seguir mostraremos como fazer a limpeza de um computador corretamente</h1>
-
-    </div>
-    <hr> 
-    <div class="promocao">
-        <div class="img">
-            <a href="#">
-                <img src="img/1.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Abrimos o gabinete <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/2.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Abrimos o gabinete <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/3.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos os cabos e conectores <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/4.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos o Drive de CD <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/5.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos o Drive de CD <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/6.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos o HD <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/7.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos a fonte <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/8.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos o Cooler <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/9.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos o Processador <br>
-                
-            </div> 
-        </div> 
-        <div class="img">
-            <a href="#">
-                <img src="img/10.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retiramos o Processador  <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/11.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Repondo a pasta térmica <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/12.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Repondo a pasta térmica  <br>
-              
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/13.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retirando a placa <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/14.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retirando Placa <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/15.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Limpeza <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/16.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Limpeza <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/17.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Retirar a placa para limpeza do gabinete <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/18.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-              p  Retirar a placa para limpeza do gabinete <br>
-              
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/19.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Recolocação dos componentes <br>
-             
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/20.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Recolocação dos componentes<br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/21.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Recolocação dos componentes<br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/22.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Recolocação dos componentes <br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/23.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Recolocação dos componentes <br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/24.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Reconectando os cabos<br>
-              
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/25.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Reconectando os cabos<br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/26.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Reconectando os cabos<br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/27.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Reconectando os cabos<br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/28.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Reconectando os cabos<br>
-             
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/29.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Recolocando o cooler<br>
-             
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/30.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Recolocando o cooler<br>
-                
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/31.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Recolocando o cooler<br>
-              
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/32.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                 Colocando a tampa do gabinete<br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/33.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                Colocando a tampa do gabinete<br>
-              
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/34.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                  Colocando a tampa do gabinete<br>
-               
-            </div> 
-        </div>
-        <div class="img">
-            <a href="#">
-                <img src="img/35.jpg" width="300" height="200"/>
-            </a>
-            <div class="desc">
-                  Colocando a tampa do gabinete<br>
-                
-            </div> 
-        </div>
-    </div>
+<div class="content5">
+    <big>
+        <b>
+            <div class="titulo">
+                <h1> A seguir mostraremos como fazer a limpeza de um computador corretamente</h1>
+            </div>
+            <hr> 
+            <div class="boxpromocao">
+                <div class="img">
+                    <div class="desc">
+                        Passo 1 <br>
+                        Abrimos o gabinete <br>
+                        <a href="#">
+                            <img src="img/1.jpg" width="400" height="300"/>
+                            <img src="img/2.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    <div class="desc">
+                        Passo 2 <br>
+                        Retiramos os cabos e conectores <br>
+                        <a href="#">
+                            <img src="img/3.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    <div class="desc">
+                        Passo 3 <br>
+                        Retiramos o Drive de CD <br>
+                        <a href="#">
+                            <img src="img/4.jpg" width="400" height="300"/>
+                            <img src="img/5.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    <div class="desc">
+                        Passo 4 <br>
+                        Retiramos o HD <br>
+                        <a href="#">
+                            <img src="img/6.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    <div class="desc">
+                        Passo 5 <br>
+                        Retiramos a fonte <br>
+                        <a href="#">
+                            <img src="img/7.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    <div class="desc">
+                        Passo 6 <br>
+                        Retiramos o Cooler <br>
+                        <a href="#">
+                            <img src="img/8.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    <div class="desc">
+                        Passo 7 <br>
+                        Retiramos o Processador <br>
+                        <a href="#">
+                            <img src="img/9.jpg" width="400" height="300"/>
+                            <img src="img/10.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 8 <br>
+                    <div class="desc">
+                        Repondo a pasta térmica <br>
+                        <a href="#">
+                            <img src="img/11.jpg" width="400" height="300"/>
+                            <img src="img/12.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 9 <br>
+                    <div class="desc">
+                        Retirando a placa <br>
+                        <a href="#">
+                            <img src="img/13.jpg" width="400" height="300"/>
+                            <img src="img/14.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 10 <br>
+                    <div class="desc">
+                        Limpeza <br>
+                        <a href="#">
+                            <img src="img/15.jpg" width="400" height="300"/>
+                            <img src="img/16.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 11 <br>
+                    <div class="desc">
+                        Retirar a placa para limpeza do gabinete <br>
+                        <a href="#">
+                            <img src="img/17.jpg" width="400" height="300"/>
+                            <img src="img/18.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 12 <br>
+                    <div class="desc">
+                        Recolocação dos componentes <br>
+                        <a href="#">
+                            <img src="img/19.jpg" width="400" height="300"/>
+                            <img src="img/20.jpg" width="400" height="300"/>
+                            <img src="img/21.jpg" width="400" height="300"/>
+                            <img src="img/22.jpg" width="400" height="300"/>
+                            <img src="img/23.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 13 <br>
+                    <div class="desc">
+                        Reconectando os cabos<br>
+                        <a href="#">
+                            <img src="img/24.jpg" width="400" height="300"/>
+                            <img src="img/26.jpg" width="400" height="300"/>
+                            <img src="img/27.jpg" width="400" height="300"/>
+                            <img src="img/28.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 14 <br>
+                    <div class="desc">
+                        Recolocando o cooler<br>
+                        <a href="#">
+                            <img src="img/29.jpg" width="400" height="300"/>
+                            <img src="img/31.jpg" width="400" height="300"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="img">
+                    Passo 15 <br>
+                    <div class="desc">
+                        Colocando a tampa do gabinete<br>
+                        <a href="#">
+                            <img src="img/32.jpg" width="400" height="300"/>
+                            <img src="img/33.jpg" width="400" height="300"/>
+                            <img src="img/34.jpg" width="400" height="300"/>
+                            <img src="img/35.jpg" width="400" height="300"/>
+                        </a>
+                    </div> 
+                </div>
+            </div>
+        </b>
+    </big>
 </div>

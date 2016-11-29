1571433e9a41e1b5584b7601104fb14986ed21a5
amanda_hilda <amanda_hilda@SNRSL1ELBIET034>
2016-11-29 11:42:20 -0200

    ultimas

---
+++ consultoria.php
@@ -0,0 +1,100 @@
+<div id="content1">
+    <h1 align="center">
+        <big>
+            Configurações voltadas para a classe de clientes
+        </big>
+    </h1>
+    <br>
+    <p class="parag">
+    <p> <b>  Configurações básicas </b></p>
+    <br>
+    <ol>
+        <p>CPU: AMD FX-6300</p>
+            <br>
+            <p>Placa Mãe: ASUS M5A78L LE</p>
+            <br>
+            <p>RAM: Corsair 4 GB</p>
+            <br>
+
+            <p>GPU: Power Color R7</p>
+
+            <br>
+            <p> HD: HD Seagate 1 TB</p>
+
+            <br>
+            <p>Fonte: PCYES 430W</p>
+            <br>
+            <p>Gabinete: Bitfenix Merc Alpha</p>
+            <br>
+
+            <p><b> Configuração Intermediária </b> </p>
+            <br>
+
+            <p> Processador: AMD FX-8370E </p> <br>
+            <p> Cooler: Artic Freezer A11 AMD Edition</p> <br>
+            <p> Memória RAM: Kingston HyperX Fury 16 GB 1866 MHz</p> <br>
+            <p>Placa de vídeo: PNY NVIDIA GeForce GTX 960 4 GB </p> <br>
+            <p>Placa-mãe: Gigabyte GA-990XA- UD3 </p> <br>
+            <p>Fonte de alimentação: CoolerMaster G650M FullRange </p> <br>
+            <p>Armazenamento: Seagate Barracuda 2 TB (ou SSD + HD de 1 TB) </p> <br>
+            <p>Gabinete: Cougar MX300 </p> <br>
+            <p>Fans extras: 2x Sharknoon SharkBlades 120 mm </p> <br>
+
+            <p><b> Configuração Avançada </b> </p>
+            <br>
+            <p>Processador: Intel Core i7-6700K </p> <br>
+            <p>Cooler: Corsair WaterCooler H100i Extreme Performance </p> <br>
+            <p>Placa-mãe: ASUS LGA 1151 Z170 Deluxe </p> <br>
+            <p>Memória: Kingston HyperX Fury DDR4 </p> <br>
+            <p>Placa de vídeo: ASUS GTX 980 Ti Poseidon Platinum </p> <br>
+            <p> Armazenamento: SSD + HD</p> <br>
+            <p>Gabinete: NZXT MidTower H440 </p> <br>
+            <p>Fonte de alimentação: Corsair HX1000i 80 Plus Platinum </p> <br></p>
+            <br>
+    </ol>
+</div>
+<div id="content2">
+    <h1 align="center">
+        <big>
+            Configurações voltadas para a classe de funcionários
+        </big>
+    </h1>
+    <br>
+    <p><b> Configuração Básica </b> </p>
+    <br>
+    <p>Marca: Samsung</p><br>
+    <p>Modelo: Chromebook XE500C13-AD1BR</p><br>
+    <p>Sistema Operacional: Chrome OS</p><br>
+    <p>Processador: Intel Celeron Dual Core</p><br>
+    <p>Modelo Processador: Intel Celeron N3050</p><br>
+    <p>Cache: 1.60 até 2,16 GHz, 2 MB L2 Cache</p><br>
+    <p>Chipset: Samsung</p><br>
+    <p>Memória RAM: 2 GB</p><br>
+    <p>HD: 16 GB</p><br>
+    <p>Placa de vídeo: Intel HD Graphics Integrada</p><br>
+
+    <p><b> Configuração Intermediária </b> </p>
+    <br>
+    <p>Marca: Apple</p><br>
+    <p>Sistema operacional: Mac OS</p><br>
+    <p>Marca do processador: Intel CPU</p><br>
+    <p>Velocidade do processador: 3,5 GHz</p><br>
+    <p>Processador gráfico: NVIDIA GPU</p><br>
+    <p>Memória RAM: 8GB</p><br>
+    <p>HD: 1TB</p><br>
+
+    <p><b> Configuração Avançada </b> </p>
+    <br>
+    <p> Marca: Apple</p><br>
+    <p>Processador: Intel core i7 </p><br>
+    <p>Modelo Processador: Quad core </p><br>
+    <p>Cache: 6 MB </p><br>
+    <p>Chipset: i7 </p><br>
+    <p>Memória RAM: 16GB </p><br>
+    <p>HD: 512GB </p><br>
+    <p>SSD: 512GB </p><br>
+
+</div>
+<div id="content3">
+
+</div>
--- index.php
+++ index.php
@@ -18,27 +18,29 @@ and open the template in the editor.
             </div>
             <div id="menu">
                 <ul>
-                    <li><a class="<?=($_GET["pg"]=="home"||empty($_GET["pg"]))?"active":"" ?>" href="?pg=home">Home</a></li>
-                    <li><a class="<?=($_GET["pg"]=="produtos")?"active":"" ?>" href="?pg=produtos">Produtos</a></li>
-                    <li><a class="<?=($_GET["pg"]=="servicos")?"active":"" ?>" href="?pg=servicos">Serviços</a></li>
-                    <li><a class="<?=($_GET["pg"]=="contato")?"active":"" ?>" href="?pg=contato">Contato</a></li>
-                     <li><a class="<?=($_GET["pg"]=="descarte")?"active":"" ?>" href="?pg=descarte">Descarte</a></li>
-                     <li><a class="<?=($_GET["pg"]=="faq")?"active":"" ?>" href="?pg=faq">FAQ</a></li>
-                     <li><a class="<?=($_GET["pg"]=="limpeza")?"active":"" ?>" href="?pg=limpeza">Limpeza</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "home" || empty($_GET["pg"])) ? "active" : "" ?>" href="?pg=home">Home</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "produtos") ? "active" : "" ?>" href="?pg=produtos">Produtos</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "servicos") ? "active" : "" ?>" href="?pg=servicos">Serviços</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "descarte") ? "active" : "" ?>" href="?pg=descarte">Descarte</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "faq") ? "active" : "" ?>" href="?pg=faq">FAQ</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "limpeza") ? "active" : "" ?>" href="?pg=limpeza">Limpeza</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "consultoria") ? "active" : "" ?>" href="?pg=consultoria">Consultoria</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "planta") ? "active" : "" ?>" href="?pg=planta">Planta</a></li>
+                    <li><a class="<?= ($_GET["pg"] == "contato") ? "active" : "" ?>" href="?pg=contato">Contato</a></li>
                 </ul>
             </div>
             <div id="content">
                 <?php
-                    $pg = $_GET["pg"];
-                    if(!empty($pg)&&isset($pg)) {
-                        include_once $pg.'.php';
-                    } else {
-                        include_once 'home.php';
-                    }
+                $pg = $_GET["pg"];
+                if (!empty($pg) && isset($pg)) {
+                    include_once $pg . '.php';
+                } else {
+                    include_once 'home.php';
+                }
                 ?>
             </div>
             <div id="footer">
-                 <p>2016 &copy; - Todos os direitos reservados.</p>
+                <p>2016 &copy; - Todos os direitos reservados.</p>
             </div>
         </div>
 

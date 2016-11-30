5e270924f22829a66b9b4fa2d4f17f0252b6b206
amanda_hilda <amanda_hilda@SNRSL1ELBIET034>
2016-11-30 17:47:26 -0200

    aterações feitas

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ and open the template in the editor.
                 <ul>
                     <li><a class="<?= ($_GET["pg"] == "home" || empty($_GET["pg"])) ? "active" : "" ?>" href="?pg=home">Home</a></li>
                     <li><a class="<?= ($_GET["pg"] == "produtos") ? "active" : "" ?>" href="?pg=produtos">Produtos</a></li>
+                     <li><a class="<?= ($_GET["pg"] == "promocoes") ? "active" : "" ?>" href="?pg=promocoes">Promoções</a></li>
                     <li><a class="<?= ($_GET["pg"] == "servicos") ? "active" : "" ?>" href="?pg=servicos">Serviços</a></li>
                     <li><a class="<?= ($_GET["pg"] == "descarte") ? "active" : "" ?>" href="?pg=descarte">Descarte</a></li>
                     <li><a class="<?= ($_GET["pg"] == "faq") ? "active" : "" ?>" href="?pg=faq">FAQ</a></li>
---
+++ promocoes.php
@@ -0,0 +1,42 @@
+<div id="content6">
+    <div class="titulo">
+        <h1><big> Conheça nossas promoções </big></h1>
+
+    </div>
+    <hr> 
+    <div class="promocao">
+        <div class="img">
+            <a href="#">
+                <img src="img/iphone 5s.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Iphone 5s Apple<br>
+                <br>
+                De R$ 1.999,00<br>
+                 Por R$ 1.500,00
+                
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/sansung.jpg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Iphone 6s Apple<br>
+                <br>
+                De R$ 4.299,00<br>
+                 Por R$ 3.899,00
+            </div> 
+        </div>
+        <div class="img">
+            <a href="#">
+                <img src="img/6s.jpeg" width="300" height="200"/>
+            </a>
+            <div class="desc">
+                Macbook Air MJVG2BZ/A<br>
+                <br>
+                De R$ 4.966,15<br>
+                Por R$ 3,900,00
+            </div> 
+        </div>
+
